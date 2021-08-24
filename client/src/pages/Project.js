d5a0c81131d56face73b979b44701716aec79075
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-08-24 16:07:37 +0100

    added project deletion

--- client/src/pages/Project.js
+++ client/src/pages/Project.js
@@ -1,8 +1,21 @@
-import { useLocation } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
+import axios from "axios";
+import { Button } from "react-bootstrap";
 
-const Project = ({projectId, projectName}) => {
+const Project = () => {
 
   const { state } = useLocation();
+  const history = useHistory();
+
+  const deleteProject = async () => {
+    await axios.delete(`/projects/${state["project_id"]}`,{}).then((result) => {
+      console.log("result: ", result);
+    }).catch((error) => {
+      console.log("error: ", error);
+    }).finally(() => {
+      history.push("/projects");
+    });
+  }
 
   return (
     <div>
@@ -10,9 +23,14 @@ const Project = ({projectId, projectName}) => {
         <>
           <h1>Project: {state["project_id"]}</h1>
           <h2>Name: {state.name}</h2>
+          <Button onClick={() => history.push("/projects")}>Back to Projects</Button>
+          <Button variant={"danger"} onClick={deleteProject}>Delete</Button>
         </>
       ) : (
-        <h1>Could not get state from path</h1>
+        <>
+          <h1>Could not get state from path</h1>
+          <Button onClick={() => history.push("/projects")}>Back to Projects</Button>
+        </>
       )}
     </div>
   )
