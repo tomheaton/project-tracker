4d1e78153ac042b18a1baf719166207b7630baa9
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-09-25 16:54:20 +0100

    add session provider :moon:

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css';
 import type {AppProps} from 'next/app';
+import {SessionProvider} from "next-auth/client";
+import React from 'react';
 
-const MyApp = ({Component, pageProps}: AppProps) => {
-    return <Component {...pageProps} />
+const MyApp = ({Component, pageProps: {session, ...pageProps}}: AppProps) => {
+    return (
+        <SessionProvider>
+            <Component {...pageProps} />
+        </SessionProvider>
+    );
 }
 
 export default MyApp;
