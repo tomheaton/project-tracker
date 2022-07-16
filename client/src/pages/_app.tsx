ed52966864ea1407d94c7547affc11d8060bbdaf
Tom Heaton <thomas.m.heaton@gmail.com>
2022-07-16 22:22:52 +0100

    client work

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,13 +1,10 @@
 import '../styles/globals.css';
 import React from 'react';
-import { SessionProvider } from "next-auth/react"
 import {AppProps} from "next/app";
 
 const MyApp = ({ Component, pageProps: { session, ...pageProps }}: AppProps) => {
     return (
-        <SessionProvider session={session}>
-            <Component {...pageProps} />
-        </SessionProvider>
+        <Component {...pageProps} />
     );
 }
 
--- client/src/pages/api/auth/logout.ts
+++ client/src/pages/api/auth/logout.ts
@@ -1,11 +1,10 @@
-import type {NextApiRequest, NextApiResponse} from 'next';
+import {NextApiRequest, NextApiResponse} from 'next';
 
 type Data = {
     name: string
 }
 
 const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
-    console.log("logged out");
     res.status(200).json({name: 'Tom Heaton'});
 }
 
